20d8b15505d861feff40d25f04384a1fb6bba94c
NahomM12 <151675573+NahomM12@users.noreply.github.com>
2026-08-13 23:41:31 +0300

    imported user

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
 
 class SuperAdminSeeder extends Seeder
 {
